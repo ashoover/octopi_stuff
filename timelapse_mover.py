60a5a5f1760d46c42a6210ff801f55e2ab171093
Adam Hoover <ashoover@gmail.com>
2018-02-08 00:59:13 -0600

    Update timelapse_mover.py

--- timelapse_mover.py
+++ timelapse_mover.py
@@ -14,8 +14,6 @@ sleep_time = 600
 url = "http://octopi/api/job"
 headers = {'x-api-key': api_key}
 response_json = requests.get(url, headers=headers).json()
-printer_progress = response_json["progress"]["completion"]
-printer_state = response_json["state"]
 
 def tl_push():
 
@@ -25,25 +23,15 @@ def tl_push():
                 full_filename = timelapse_source + filename
                 shutil.move(full_filename, timelapse_destination)
 
-        print("Files Moved to NAS.")
-
-
     if printer_state == "Operational":
-        print("Printer is online.")
-
         while printer_progress != None:
-            sleep_minutes = int(sleep_time / 60)
-            print("Printer is online and printing.  Rechecking in {} minutes.".format(sleep_minutes))
-            time.sleep(1)
+            time.sleep(sleep_time)
 
         else:
-            print("Printer is not printing.  Moving files to NAS.")
             tl_mover()
 
 
     else:
         print("Printer is offline. Stopping script.")
 
-print("Printer State is : {}".format(printer_state))
-print("Printer Progress is : {}".format(printer_progress))
 tl_push()
